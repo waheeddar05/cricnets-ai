832edc5f15ce032463c9db9c62ac80195092c4d7
waheeddar05 <waheed.dar05@gmail.com>
2026-01-14 11:17:30 +0530

    Initial commit

---
+++ src/main/java/com/wam/cricnets_ai/CricnetsAiApplication.java
@@ -0,0 +1,13 @@
+package com.wam.cricnets_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricnetsAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricnetsAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wam/cricnets_ai/CricnetsAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wam.cricnets_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CricnetsAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
