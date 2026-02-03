98111c0f0da301b01b8d03ad289e40c25b897c23
waheeddar05 <waheed.dar05@gmail.com>
2026-02-03 23:31:31 +0530

    fix: exclude management security auto-config

--- src/main/java/com/wam/cricnets_ai/CricnetsAiApplication.java
+++ src/main/java/com/wam/cricnets_ai/CricnetsAiApplication.java
@@ -1,9 +1,10 @@
 package com.wam.cricnets_ai;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = ManagementWebSecurityAutoConfiguration.class)
 public class CricnetsAiApplication {
 
 	public static void main(String[] args) {
